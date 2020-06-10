bb8d2825e495143adf14b2a9c92c3c5c9aea1cee
lajoskovago <kovago.lajos91@gmail.com>
2020-06-10 17:46:08 +0300

    initial commit for aptor

---
+++ app/Http/Controllers/HotelController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Hotel;
+use Illuminate\Http\Request;
+
+class HotelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $hotels = Hotel::latest()->paginate(5);
+
+        return view('hotels.index',compact('hotels'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('hotels.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nume' => 'required',
+            'email' => 'required',
+            'nume_contact' => 'required',
+            'email_contact' => 'required',
+        ]);
+
+        Hotel::create($request->all());
+
+        return redirect()->route('hotels.index')
+                        ->with('success','Product created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Hotel  $hotel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(hotel $hotel)
+    {
+        return view('hotels.show',compact('hotel'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Hotel  $hotel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Hotel $hotel)
+    {
+        return view('hotels.edit',compact('hotel'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Hotel  $hotel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Hotel $hotel)
+    {
+        $request->validate([
+            'name' => 'required',
+            'detail' => 'required',
+        ]);
+
+        $hotel->update($request->all());
+
+        return redirect()->route('hotels.index')
+                        ->with('success','Hotel updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Hotel  $hotel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Hotel $hotel)
+    {
+        $hotel->delete();
+
+        return redirect()->route('hotels.index')
+                        ->with('success','Product deleted successfully');
+    }
+}
